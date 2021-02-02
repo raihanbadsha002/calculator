e64518cb005cc06508704c7d75b1ed9a6fcd153d
RAIHAN <raihanbadsha363@gmail.com>
2021-02-02 13:50:09 +0600

    calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+let display = document.querySelector('form input'),
+    button = document.querySelectorAll('.buttons button');
+
+    function fillIt(num) {
+        display.value = display.value + num;
+    }
+    function clearIt() {
+        display.value = "";
+    }
+    function deleteIt() {
+        let screen = document.querySelector('form input').value;
+        display.value = display.value.substring(0, screen.length -1);
+    }
+    function calculateIt() {
+        display.value = eval(display.value);
+    }
